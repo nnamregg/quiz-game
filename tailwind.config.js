f5922fbbc8cefcd96779bb1107d8e89f96565265
German Troncoso <nnamreg.t@gmail.com>
2023-10-05 12:33:43 -0300

    Bump deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-    content: ["./public/**/*.html", "./src/**/*.vue"],
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-        extend: {
-            fontFamily: {
-                'sans': ['Barlow', 'sans-serif']
-            },
-            colors: {
-                current: 'currentColor',
-                teal: colors.teal,
-                red: colors.red,
-                green: colors.green,
-                orange: colors.orange
-            },
-        },
+  content: ["./public/**/*.html", "./src/**/*.vue"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Barlow", "sans-serif"],
+      },
+      colors: {
+        current: "currentColor",
+        teal: colors.teal,
+        red: colors.red,
+        green: colors.green,
+        orange: colors.orange,
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [
-        require('@tailwindcss/forms')
-    ],
-}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
