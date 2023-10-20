816fd42351c33bdfb1f0795cd10dd0ee60d9ce10
German Troncoso <nnamreg.t@gmail.com>
2023-10-20 01:40:48 -0300

    Fix alturas y margenes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { onMounted } from "vue";
 import { useStore } from "@/stores/main";
-// import DarkModeSelector from "@/components/DarkModeSelector.vue";
+import Header from "@/components/Header.vue";
 import QuizSelection from "@/components/QuizSelection.vue";
 import Quiz from "@/components/Quiz/Main.vue";
 import FinalScore from "@/components/FinalScore.vue";
@@ -14,8 +14,9 @@ onMounted(() => {
 </script>
 
 <template>
+  <Header />
   <main
-    class="mx-auto my-8 box-border flex h-full w-full max-w-4xl overflow-hidden rounded bg-neutral-100/75 shadow-md backdrop-blur-sm hover:shadow-lg dark:bg-neutral-950/75 md:my-20 lg:my-32"
+    class="mx-auto mb-8 mt-1 box-border flex h-full w-11/12 max-w-3xl overflow-hidden rounded-md bg-neutral-100/75 shadow-md backdrop-blur-sm hover:shadow-lg dark:bg-neutral-950/75"
   >
     <QuizSelection v-if="!store.quizLength" />
 
